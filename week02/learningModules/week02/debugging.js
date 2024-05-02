e4296d7befa3800a57a5b4f0a8f60aabe0577747
ryzen5 <ryzen5@studio-acores.Home>
2024-05-02 21:03:06 +0100

    css structure

---
+++ week02/learningModules/week02/debugging.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius){
+    const area = radius * radius *PI;
+}
+area = circleArea(3);
+console.log("area1:", area);
+
+area = circleArea(4);
+console.log("area2:", area);
