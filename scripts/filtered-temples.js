e4797339f7871ae29870346328a9fc16ec60ba94
svpontes <pontes_sergio@hotmail.com>
2024-05-16 01:38:35 +0100

    filtered-temple-starts

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,143 @@
+const filters = document.querySelectorAll('.navigation a');
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+document.addEventListener("DOMContentLoaded", function() {
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    filters.forEach(filter => {
+        filter.addEventListener('click', () => {
+            resetFilters();
+            filter.classList.toggle('active');
+            applyFilter(filter.id.toLowerCase()); // Convertendo o ID para minúsculas
+        });
+    });
+
+    createTempleCards(temples);
+});
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+   
+];
+
+function resetFilters() {
+    filters.forEach(filter => {
+        filter.classList.remove('active');
+    });
+}
+
+function applyFilter(filterId) {
+    let filteredTemples = [];
+
+    switch (filterId) {
+        case 'home':
+            filteredTemples = temples;
+            break;
+        case 'old':
+            filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() < 1900);
+            break;
+        case 'new':
+            filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() > 2000);
+            break;
+        case 'small':
+            filteredTemples = temples.filter(temple => temple.area < 10000); // Corrigindo o valor do filtro "Small"
+            break;
+        case 'large':
+            filteredTemples = temples.filter(temple => temple.area > 90000);
+            break;
+        default:
+            filteredTemples = temples;
+            break;
+    }
+
+    createTempleCards(filteredTemples);
+}
+
+function createTempleCards(temples) {
+    const grid = document.querySelector('.res-grid');
+    grid.innerHTML = '';
+
+    temples.forEach(temple => {
+        const card = document.createElement('section');
+        const name = document.createElement('h3');
+        const location = document.createElement('p');
+        const dedication = document.createElement('p');
+        const area = document.createElement('p');
+        const img = document.createElement('img');
+
+        name.textContent = temple.templeName;
+        location.innerHTML = `<span class='label'>Location: </span> ${temple.location}`;
+        dedication.innerHTML = `<span class='label'>Dedicated: </span> ${temple.dedicated}`;
+        area.innerHTML = `<span class='label'>Size: </span> ${temple.area} sq ft.`;
+        img.src = temple.imageUrl;
+        img.alt = `${temple.templeName} Temple`;
+        img.loading = 'lazy';
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        grid.appendChild(card);
+    });
+}
--- week4/learningContents/objectsinjavascript.js
+++ week4/learningContents/objectsinjavascript.js
@@ -9,7 +9,7 @@ let person = {
         country: "Honduras"
     },
     isEmployed: true,
-    greet: function(){
+    greet: function(){//short notation greet(){console.log('Hello, my name is ${this.name}');}
         console.log('Hello, my name is ${this.name}');
     }
 };
@@ -29,3 +29,22 @@ let anAmount = Albuterol[amount];
 Albuterol[amount]=1455;
 
 let validation = anAmount.Rosuvastatin.expDate;
+/*Using the array.filter() method, create an array named namesB with only those names 
+from the name array that start with the character 'B'.
+*/
+let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
+let namesb = names.filter(name => name.charAt(0) ==='B');
+
+/*Using the array.map() method, create a new array named namesLength that contains the 
+length of each name in the names array. Expected output is [5, 8, 5, 8, 3].*/
+
+let namesLenght = names.map((name) => name.length);
+
+/*Using the names.reduce() method, create an expression that returns the average string length
+ of the names in the names array. Expected output is 5.8.*/
+
+ names.reduce((total, name)=> total + name.length, 0)/names.length;
+ //0 initial value of the reduction
+ //total epresents the accumulated sum of string lengths
+ //name represents each individual name in the array
+ //reducer function adds the length of each name to the total in each iteration
